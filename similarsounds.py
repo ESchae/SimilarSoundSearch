ec3660ef2946b456c120f253b63998691e60f47d
ESchae <elke.schaechtele@web.de>
2016-06-07 22:46:49 +0200

    added new argument to main module

--- similarsounds.py
+++ similarsounds.py
@@ -12,6 +12,7 @@ import os
 from subprocess import call
 from time import sleep
 import random
+import itertools
 input = getattr(__builtins__, 'raw_input', input)  # outsmart checkstyle
 
 
@@ -83,59 +84,101 @@ def display_result(i, n, path, distance, listening=False):
 
 if __name__ == "__main__":
     # console arguments handling
-    import itertools
-    queries = itertools.cycle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
     import argparse
     parser = argparse.ArgumentParser(
-        description='A very helpful description...',
-        epilog='...  have fun!')
-    parser.add_argument('S', type=str,
-                        help="""path to a sql database for sounds (aka
-                                soundbase); can be build with the help
-                                of the SoundBase class""")
-    parser.add_argument('-q', '--q', type=str,
-                        help='a query audiofile')
+        description='A small command line query-by-example search engine for \
+similar sounds.',
+        epilog='...  turn on your speakers or headphones and have fun!')
+    parser.add_argument('soundbase', type=str,
+                        help='path to a sql database for sounds built with \
+soundbase.py')
+    parser.add_argument('-query', type=str,
+                        help='filename of a query audiofile, if no query is \
+given an audiofile will be randomly selected from the soundbase')
     parser.add_argument('-n', type=int,
-                        help='number of results to be returned', default=14)
-    parser.add_argument('-o', type=str,
-                        help='print results additionally to this file')
+                        help='number of results to be returned, default: 14',
+                        default=14)
+    parser.add_argument('-output', type=str,
+                        help='name of a file to which results should be \
+printed additionally')
+    parser.add_argument('-D1', action='store_true',
+                        help='each time you use "r" for a new query, \
+one of the ten queries from D1 will be used, must be used together with \
+D1.db')
+    parser.add_argument('-features', type=str,
+                        help='which features to include, possible options: \
+pitch, pitchConfidence, mfcc, spectralcnetroid, duration, logattacktime, \
+dynamicComplexity, effectiveDuration, loudness (use without blank and \
+comma separated)')
+    parser.add_argument('-stats', type=str,
+                        help='which statistics should be used for feature \
+aggregation, possible options: mean, var, dmean, dvar (use without blank and \
+comma separated)')
+    parser.add_argument('-nbrsin', type=str,
+                        help='input filename for saved search space')
+    parser.add_argument('-sclrin', type=str,
+                        help='input filename for saved scaler')
+    parser.add_argument('-nbrsout', type=str,
+                        help='output filename for saving search space')
+    parser.add_argument('-sclrout', type=str,
+                        help='output filename for saving scaler')
     args = parser.parse_args()
 
     # ------------------------ preliminaries --------------------------- #
 
     # check if audiofile is valid if a query is given
-    if args.q:
-        test_audiofile_validity(args.q)
+    if args.query:
+        test_audiofile_validity(args.query)
 
     # check if soundbase exists
-    if not os.path.isfile(args.S):
-        raise IOError("SoundBase %s does not exist" % args.S)
+    if not os.path.isfile(args.soundbase):
+        raise IOError("SoundBase %s does not exist" % args.soundbase)
+
+    if args.D1 and (args.soundbase != 'D1.db'):
+        raise IOError(
+            'option -D1 can only be used if D1.db is used as soundbase')
 
     # --------------- begin of interactive communication ------------------- #
+
+    # information on soundbase and start connection
+    database = args.soundbase
+    db = SoundBase(database)
+    max_rows = db.num_rows()
+
+    # build search space
+    if args.features:
+        features = args.features.split(',')
+    else:
+        features = ''
+    if args.stats:
+        stats = args.stats.split(',')
+    else:
+        stats = ''
+    s = Search(database, n=args.n, features=features,
+               nbrs_input=args.nbrsin, nbrs_output=args.nbrsout,
+               scaler_input=args.sclrin, scaler_output=args.sclrout)
+
+    # get query
+    if not args.query:
+        random_mode = True
+        if args.D1:
+            # iterate over all queries
+            queries = itertools.cycle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+            query_id = queries.next()
+        else:
+            query_id = random.randint(1, max_rows)
+        query = db.get_row(query_id)['path']
+    else:
+        random_mode = False
+        query = args.query
 
     # print header
     print("\033[H\033[2J")  # erase display
     print("\033[1;32mWelcome to SimilarSoundsSearch!\033[0m")
     print()
-
     # print database
     print("\033[1;32mYour soundbase: \033[0m", end='')
-    database = args.S
-    db = SoundBase(database)
-    max_rows = db.num_rows()
     print(database)
-
-    s = Search(database, n=args.n)
-
-    # get query
-    if not args.q:
-        random_mode = True
-        query_id = random.randint(1, max_rows)
-        query_id = queries.next()
-        query = db.get_row(query_id)['path']
-    else:
-        random_mode = False
-        query = args.q
 
     # print query
     display_query(query, random_mode=random_mode, listening=False)
@@ -145,9 +188,9 @@ if __name__ == "__main__":
     print("\033[1;32mResult: \033[0m")
     s.print_result(result)
 
-    if args.o:
+    if args.output:
         # write result to file
-        with open(args.o, 'a') as f:
+        with open(args.output, 'a') as f:
             f.write("Query: %s\n" % query)
             f.write("Result: \n")
             s.print_result(result, output=f)
@@ -217,8 +260,10 @@ if __name__ == "__main__":
             try:
                 query = input("New query file or r(andom): ")
                 if query == 'r':  # randomly select new query
-                    # query_id = random.randint(1, 10)
-                    query_id = queries.next()
+                    if args.D1:
+                        query_id = queries.next()
+                    else:
+                        query_id = random.randint(1, 10)
                     query = db.get_row(query_id)['path']
                     random_mode = True
                 # go to line and erase screen under it
@@ -229,9 +274,9 @@ if __name__ == "__main__":
                 result = s.query(query)
                 print("\033[6;0H\033[1;32mResult: \033[0m")
                 s.print_result(result)
-                if args.o:
+                if args.output:
                     # write result to file for evaluating D2
-                    with open(args.o, 'a') as f:
+                    with open(args.output, 'a') as f:
                         f.write("Query: %s\n" % query)
                         f.write("Result: \n")
                         s.print_result(result, output=f)
